81603a30812b24dce5e1e921d3a0fbc8a617c2d9
connect-aima <aimaabbas301@gmail.com>
2026-01-18 23:37:33 +0500

    added more to middleware.ts

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -10,8 +10,20 @@ import { NextRequest } from "next/server";
 //     matcher: ['/profile'],
 // };
 //method 2 conditional statement 
+// export function middleware(request: NextRequest) {
+    
+//     if (request.nextUrl.pathname.startsWith('/profile')) {
+//         return NextResponse.redirect(new URL('/hello', request.url));
+//     }
+// }
+// writing custom cookies and headers in middleware
 export function middleware(request: NextRequest) {
-    if (request.nextUrl.pathname.startsWith('/profile')) {
-        return NextResponse.redirect(new URL('/hello', request.url));
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get('theme');
+    if (!themePreference) {
+        response.cookies.set('theme', "dark");
     }
+    response.cookies.set('middleware-cookie', 'cookie-value-from-middleware');
+    response.headers.set('X-Middleware-Header', 'header-value-from-middleware');
+    return response;
 }
